e66e8ade3a0a108e428f23b048f15543ed6116e4
RFaria <renatameneses12@gmail.com>
2017-12-29 18:52:34 -0100

    added id

--- src/whoiswho/Characters.java
+++ src/whoiswho/Characters.java
@@ -4,10 +4,12 @@ public class Characters {
     private String name;
     private boolean selected = false;
     private String gender;
+    private int id;
 
-    public Characters(String name, String gender){
+    public Characters(String name, String gender, int id){
         this.name = name;
         this.gender = gender;
+        this.id = id;
     }
 
     public String getName() {
@@ -16,6 +18,10 @@ public class Characters {
 
     public String getGender(){
         return gender;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public boolean isSelected() {
